644e81ec9cdca7612aa7351d3481e8c20923bd58
Test <justnisar@gmail.com>
2019-04-01 00:22:06 -0700

    Updated some code on sunday

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ class App extends Component {
   state = {
     persons : [
       {
+        id: '1',
         name: "Nisar",
         age: 29
       },
       {
+        id: '2',
         name: "Vikus",
         age: 30
       },
       {
+        id: '3',
         name: "Shanky",
         age: 30
       }
@@ -27,17 +30,17 @@ class App extends Component {
     this.setState({
       persons : [
         {
-          id: 'sdsd',
+          id: '1',
           name: newName,
           age: 30
         },
         {
-          id: 'sdr',
+          id: '2',
           name: "Vikus",
           age: 30
         },
         {
-          id: 'awe',
+          id: '3',
           name: "Shanky",
           age: 30
         }
@@ -46,24 +49,24 @@ class App extends Component {
   );
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons : [
-        {
-          name: 'Max',
-          age: 30
-        },
-        {
-          name: event.target.value,
-          age: 30
-        },
-        {
-          name: "Shanky",
-          age: 30
-        }
-      ]
-    }
-  );
+      persons : persons
+    });
+
   }
 
   togglePersonshandler = () => {
@@ -75,12 +78,24 @@ class App extends Component {
   }
 
   deletePersonHandler = (personIndex) => {
-    const persons = this.state.persons;
+    // fetch all persons and update new state
+    const persons = [...this.state.persons];
     persons.splice(personIndex,1);
-    this.setState({
-      persons : persons
-    });
+    this.setState({persons : persons});
   }
+
+   maxScoreSightseeingPair = function(A) {
+    let max = 0;
+    for(let i = 0 ; i < A.length - 1 ; i++){
+      for(let j = i + 1 ; j < A.length ; j++){
+        let current = A[i] + A[j] + i - j;
+        if(current > max){
+          current = max;
+        }
+      }
+    }
+    return max;
+};
 
   render() {
 
@@ -98,12 +113,20 @@ class App extends Component {
         <div>
 
           {
-            this.state.persons.map( (person,index) => {
-              return <Person name={person.name} age={person.age} click={() => this.deletePersonHandler(index)} change={this.nameChangedHandler} key={person.id}/>
-            })
+            this.state.persons.map((person,index) => {
+              return <Person 
+                name={person.name} 
+                age={person.age}
+                click={() => this.deletePersonHandler(index)}
+                key={person.id}
+                changed={(event) => this.nameChangedHandler(event,person.id)}
+                  />
+            }
+            )
           }
 
           {/*
+       This can be replaced with map function in JS
        <Person 
        name={this.state.persons[0].name} 
        age={this.state.persons[0].age}/>
